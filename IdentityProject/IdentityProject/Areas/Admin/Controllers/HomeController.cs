e3fc8462e3d0807425b49c9cfdd6f7ba6c9b3cd5
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-01-13 22:47:36 +0300

    feat(Areas): add admin userList page

--- IdentityProject/IdentityProject/Areas/Admin/Controllers/HomeController.cs
+++ IdentityProject/IdentityProject/Areas/Admin/Controllers/HomeController.cs
@@ -1,18 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityProject.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly IUserService _userService;
+
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult UserList()
+        public async Task<IActionResult> UserList()
         {
-            return View();
+            var userViewModelList = await _userService.GetUserListAsync();
+            return View(userViewModelList);
         }
     }
 }
