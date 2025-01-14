9ef28716751fd8977d099177eac3e2f27f2eea39
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-01-14 18:16:24 +0300

    feat(login): add login page design

---
+++ IdentityProject/IdentityProject/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+    }
+}
