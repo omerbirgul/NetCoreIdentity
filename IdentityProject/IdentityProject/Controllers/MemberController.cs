22b91516502c2eb6840c67704d71fd43fe2544af
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2025-01-19 20:18:01 +0300

    feat: add change password method

--- IdentityProject/IdentityProject/Controllers/MemberController.cs
+++ IdentityProject/IdentityProject/Controllers/MemberController.cs
@@ -41,5 +41,43 @@ namespace IdentityProject.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Invalid data");
+                return View();
+            }
+
+            var hasUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            if(hasUser is null)
+            {
+                ModelState.AddModelError("", "User not found");
+                return View();
+            }
+
+            var checkOldPassword = await _userManager.CheckPasswordAsync(hasUser, model.OldPassword);
+            if (!checkOldPassword)
+            {
+                ModelState.AddModelError("", "Old password is wrong");
+                return View();
+            }
+
+            var identityResult = await _userManager.ChangePasswordAsync(hasUser,model.OldPassword,model.NewPassword);
+            if (!identityResult.Succeeded)
+            {
+                ModelState.AddModelError("", "Password could not be changed");
+                return View();
+            }
+
+            await _userManager.UpdateSecurityStampAsync(hasUser);
+            await _signInManager.SignOutAsync();
+            await _signInManager.PasswordSignInAsync(hasUser, model.NewPassword, true, false);
+
+            ViewData["Success"] = "Password changed successfully";
+            return View();
+        }
     }
 }
